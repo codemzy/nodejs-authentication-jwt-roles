0db8eb28416266a2a6c46e7d7ea2327070cdb420
Emma J <codemzy@gmail.com>
2016-09-02 13:49:47 +0000

    Added mongo connection to server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+var mongo = require('mongodb').MongoClient;
+// const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 app.use(express.static('public'));
 
 // DB setup
-mongoose.connect('mongodb://localhost:27017/auth');
+// mongoose.connect('mongodb://localhost:27017/auth');
 
 // use morgan for logging errors
 app.use(morgan('combined'));
@@ -14,11 +17,20 @@ app.use(morgan('combined'));
 // set the port
 app.set('port', (process.env.PORT || 8080));
 
-// routes
-const routes = require('./routes/routes.js');
-routes(app);
+mongo.connect(process.env.MONGO_URL, function (error, db) {
+    if (error) {
+    throw new Error('Database failed to connect!');
+    } else {
+    console.log('MongoDB successfully connected on port 27017.');
+    }
+    
+    // routes
+    // const routes = require('./routes/routes.js');
+    // routes(app);
+    
+    // start the server
+    app.listen(app.get('port'), function() {
+        console.log('Express server listening on port', app.get('port'));
+    });
 
-// start the server
-app.listen(app.get('port'), function() {
-    console.log('Express server listening on port', app.get('port'));
 });

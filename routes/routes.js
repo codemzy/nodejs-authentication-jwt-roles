2c7d5714c14832cd3eca8c3027761d5ee8a74b41
Emma J <codemzy@gmail.com>
2016-09-05 16:07:04 +0000

    Updated routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -26,24 +26,6 @@ module.exports = function (app) {
     // allow requests from cross origin
     // app.use(cors(corsOptions));
     
-    // // SIGN UP
-    // // take user data and create user in DB
-    // app.route('/signup')
-    //     // to recieve post requests from signup form
-    //     .post(jsonParser, Authentication.signup);
-    
-    // // SIGN IN    
-    // // take user data and check user exists in DB
-    // app.route('/signin')
-    //     // to recieve post requests from signin form
-    //     .post(jsonParser, requireSignIn, Authentication.signin);
-        
-    // // FORGOT PASSWORD
-    // // check user email exists in DB
-    // app.route('/forgotten')
-    //     // to recieve post requests from forgotton pw form
-    //     .post(jsonParser, Authentication.forgotpw);
-    
     // USER ROUTES
     var userRoutes = require('./user_routes');
     app.use('/user', userRoutes);

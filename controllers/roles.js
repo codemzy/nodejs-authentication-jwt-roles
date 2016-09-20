c1baffce7a1d9ad4624448b6a5e1ced678fc465a
Emma J <codemzy@gmail.com>
2016-09-20 15:24:22 +0000

    Updated roles to allow for addRole without update to DB

--- controllers/roles.js
+++ controllers/roles.js
@@ -13,7 +13,33 @@ const ROLES = {
     3: "Trial"
 };
 
+// Add role and return new permissions object so we can update the DB later in the callback
+// use this if have other updates to do to reduce calls to the DB
 exports.addRole = function(role, user, callback) {
+    const NOW = new Date().getTime();
+    let permissions = {
+        updatedAt: NOW,
+        roles: user.permissions.roles || []
+    };
+    // make sure the role is a number and exists in the roles object
+    if (!role || !validate.checkNum(role) || !ROLES[role]) {
+        // error
+        return callback("Invalid user or role");
+    }
+    // check the user info is present and of valid types
+    if (!user || !validate.checkEmail(user.email) || !validate.checkArr(permissions.roles)) {
+        // error
+        return callback("Invalid user or role");
+    }
+    // add the new role to the user roles
+    permissions.roles.push(ROLES[role]);
+    // return the callback with the new permissions object
+    return callback(null, permissions);
+};
+
+// Add role and update the DB
+// Use this if no other updates to do for this user
+exports.addRoleDB = function(role, user, callback) {
     const NOW = new Date().getTime();
     let permissions = {
         updatedAt: NOW,
@@ -36,12 +62,12 @@ exports.addRole = function(role, user, callback) {
         if (err) {
             return callback(err);
         }
-        // Callback with user roles
+        // Callback with roles
         return callback(null, permissions.roles);
     });
 };
 
-exports.deleteRole = function(role, user, callback) {
+exports.deleteRoleDB = function(role, user, callback) {
     const NOW = new Date().getTime();
     let permissions = {
         updatedAt: NOW,

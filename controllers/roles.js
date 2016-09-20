aae38fc19f48a5ef64a936d5500c3ab3b89c12a5
Emma J <codemzy@gmail.com>
2016-09-20 15:41:11 +0000

    Updated addRole to not take a callback

--- controllers/roles.js
+++ controllers/roles.js
@@ -13,28 +13,28 @@ const ROLES = {
     3: "Trial"
 };
 
-// Add role and return new permissions object so we can update the DB later in the callback
+// Add role and return new permissions object so we can update the DB later
 // use this if have other updates to do to reduce calls to the DB
-exports.addRole = function(role, user, callback) {
+exports.addRole = function(role, user) {
     const NOW = new Date().getTime();
     let permissions = {
         updatedAt: NOW,
         roles: user.permissions.roles || []
     };
-    // make sure the role is a number and exists in the roles object
-    if (!role || !validate.checkNum(role) || !ROLES[role]) {
-        // error
-        return callback("Invalid user or role");
-    }
     // check the user info is present and of valid types
     if (!user || !validate.checkEmail(user.email) || !validate.checkArr(permissions.roles)) {
-        // error
-        return callback("Invalid user or role");
+        // error - just return an empty object
+        return {};
+    }
+    // make sure the role is a number and exists in the roles object
+    if (!role || !validate.checkNum(role) || !ROLES[role]) {
+        // error - just return existing permissions object
+        return permissions;
     }
     // add the new role to the user roles
     permissions.roles.push(ROLES[role]);
     // return the callback with the new permissions object
-    return callback(null, permissions);
+    return permissions;
 };
 
 // Add role and update the DB

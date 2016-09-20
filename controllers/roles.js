e13ac21af3b62795fd0b5258afa8eba2ac22958c
Emma J <codemzy@gmail.com>
2016-09-20 14:41:12 +0000

    addRole function to roles

--- controllers/roles.js
+++ controllers/roles.js
@@ -13,11 +13,30 @@ const ROLES = {
     3: "Trial"
 };
 
-exports.addRole = function(role, callback) {
+exports.addRole = function(role, user, callback) {
+    const NOW = new Date().getTime();
+    let permissions = {
+        updatedAt: NOW,
+        roles: user.permissions.roles || []
+    };
     // make sure the role is a number and exists in the roles object
-    if (!validate.checkNum(role) || !ROLES[role]) {
+    if (!role || !validate.checkNum(role) || !ROLES[role]) {
         // error
-        return callback("Invalid role");
+        return callback("Invalid user or role");
     }
-    
+    // check the user info is present and of valid types
+    if (!user || !validate.checkEmail(user.email) || !validate.checkArr(permissions.roles)) {
+        // error
+        return callback("Invalid user or role");
+    }
+    // add the new role to the user roles
+    permissions.roles.push(ROLES[role]);
+    // update the db
+    db.collection('users').updateOne({ email: user.email }, { $set: { "permissions" : permissions } }, function(err, updated) {
+        if (err) {
+            return callback(err);
+        }
+        // Callback with user roles
+        return callback(null, permissions.roles);
+    });
 };
